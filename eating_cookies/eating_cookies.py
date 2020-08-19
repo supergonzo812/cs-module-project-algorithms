d7723b85a72700b200ca5b7b9e519f998ccfa4df
Chris <chris.gonzales.82@gmail.com>
2020-08-19 14:40:06 -0700

    single number search implemented

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,9 +3,31 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
-    # Your code here
+#     Cookie Monster can eat either 1, 2, or 3 cookies at a time. If he were given a jar of cookies with `n` cookies inside of it, how many ways could he eat all `n` cookies in the cookie jar? Implement a function `eating_cookies` that counts the number of possible ways Cookie Monster can eat all of the cookies in the jar. 
 
-    pass
+# For example, for a jar of cookies with `n = 3` (the jar has 3 cookies inside it), there are 4 possible ways for Cookie Monster to eat all the cookies inside it:
+
+#  1. He can eat 1 cookie at a time 3 times
+#  2. He can eat 1 cookie, then 2 cookies 
+#  3. He can eat 2 cookies, then 1 cookie
+#  4. He can eat 3 cookies all at once. 
+
+# Thus, `eating_cookies(3)` should return an answer of 4.
+
+1, 1, 1, 1, 1
+1, 1, 1, 2
+1, 1, 3
+1, 2, 2
+1, 3, 1
+1, 1, 2, 1
+1, 2, 1, 1
+2, 1, 1, 1
+2, 1, 2
+2, 2, 1
+3, 1, 1
+2, 3
+3, 2
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,10 +3,75 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
 
-    pass
+    if len(arr) > 1:
 
+        reference_value = arr[0]
+        reference_index = 0
+
+        while len(arr) > 1:
+            
+            for index, value in enumerate(arr): 
+                if value == reference_value :
+                    arr.pop(index)
+                    arr.pop(reference_index)
+                reference_index = index
+                reference_value = value
+
+    else:
+        return arr
+    return arr
+
+
+
+    # # check if arr has more than one element, otherwise return the array
+    # if len(arr) > 1:
+
+    #     # loop through the array from the second indext to the last element
+    #     for index in range(1, len(arr) - 1):
+
+    #         if  arr[index] == arr[0]:
+    #             arr.pop(index)
+    #             single_number(arr)
+    # else:
+    #     return arr
+
+    # return arr
+
+# [ 4, 1, 2, 1, 2]
+
+# [4, 1, 2, 1, 2]
+
+# [4, 2, 2]
+
+# [4]
+
+# [ 4, 1, 2, 1, 2]
+
+# [4, 1, 2, 1, 2]
+
+# [4, 2, 2]
+
+# [4]
+
+
+# Given a non-empty array of integers where every element appears twice except for one. 
+# Find that single number. 
+# 
+# You may assume that there will _always_ be _one_ odd-number-out and every other number in the input shows up exactly twice.  
+
+# ## Examples
+# ```
+# Sample input: [2, 2, 1]
+# Expected output: 1
+# ```
+
+# ```
+# Sample iput: [4, 1, 2, 1, 2]
+# Expected output: 4
+# ```
+
+# Can you implement a solution that finds the single number in _one_ pass through the input array with `O(1)` space complexity?
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
